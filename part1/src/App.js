d141bc7c3eee7693bd7c68739b74d3cccd012e56
thisisnguyennhatquang <92165891+thisisnguyennhatquang@users.noreply.github.com>
2023-03-10 15:34:25 -0500

    Complete code for 1.2.

--- part1/src/App.js
+++ part1/src/App.js
@@ -7,13 +7,33 @@ const Header = (props) => {
   );
 };
 
-const Content = (props) => {
+const Part = (props) => {
   console.log(props);
   return (
     <div>
       <p>
         {props.part} {props.exercises}
       </p>
+    </div>
+  );
+};
+
+const Content = (props) => {
+  console.log(props);
+  return (
+    <div>
+      <Part
+        part={props.contents[0].part}
+        exercises={props.contents[0].exercises}
+      />
+      <Part
+        part={props.contents[1].part}
+        exercises={props.contents[1].exercises}
+      />
+      <Part
+        part={props.contents[2].part}
+        exercises={props.contents[2].exercises}
+      />
     </div>
   );
 };
@@ -39,12 +59,16 @@ const App = () => {
   const part3 = "State of a component";
   const exercises3 = 14;
 
+  const contents = [
+    { part: part1, exercises: exercises1 },
+    { part: part2, exercises: exercises2 },
+    { part: part3, exercises: exercises3 },
+  ];
+
   return (
     <div>
       <Header course={course} />
-      <Content part={part1} exercises={exercises1} />
-      <Content part={part2} exercises={exercises2} />
-      <Content part={part3} exercises={exercises3} />
+      <Content contents={contents} />
       <Total
         exercises1={exercises1}
         exercises2={exercises2}
