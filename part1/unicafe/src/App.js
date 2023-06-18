1cd1d6e98db0edbd1ee3013a34c56231b0ddec66
thisisnguyennhatquang <92165891+thisisnguyennhatquang@users.noreply.github.com>
2023-06-18 15:35:20 +0700

    Added updated constants

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -18,21 +18,23 @@ const Display = (props) => {
 };
 
 const App = () => {
-  // save clicks of each button to its own state
   const [good, setGood] = useState(0);
   const [neutral, setNeutral] = useState(0);
   const [bad, setBad] = useState(0);
 
   const handleGoodClick = () => {
-    setGood(good + 1);
+    const updatedGood = good + 1;
+    setGood(updatedGood);
   };
 
   const handleNeutralClick = () => {
-    setNeutral(neutral + 1);
+    const updatedNeutral = neutral + 1;
+    setNeutral(updatedNeutral);
   };
 
   const handleBadClick = () => {
-    setBad(bad + 1);
+    const updatedBad = bad + 1;
+    setBad(updatedBad);
   };
 
   return (
