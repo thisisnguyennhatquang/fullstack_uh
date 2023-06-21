7191a8f661dce3b4b5581736d17388005960e987
thisisnguyennhatquang <92165891+thisisnguyennhatquang@users.noreply.github.com>
2023-06-21 22:12:20 +0700

    Completed courseinfo step6 (2.1)

--- part1/courseinfo/src/components/Course.js
+++ part1/courseinfo/src/components/Course.js
@@ -1,44 +1,30 @@
-const Header = (props) => {
-  console.log(props);
-  return <h1>{props.course}</h1>;
+const Header = ({ course }) => {
+  return <h1>{course}</h1>;
 };
 
-//   const Part = (props) => {
-//     console.log(props);
-//     return (
-//       <p>
-//         {props.part} {props.exercises}
-//       </p>
-//     );
-//   };
+const Part = ({ part, exercises }) => {
+  return (
+    <p>
+      {part} {exercises}
+    </p>
+  );
+};
 
-//   const Content = (props) => {
-//     console.log(props);
-//     return (
-//       <div>
-//         <Part part={props.parts[0].name} exercises={props.parts[0].exercises} />
-//         <Part part={props.parts[1].name} exercises={props.parts[1].exercises} />
-//         <Part part={props.parts[2].name} exercises={props.parts[2].exercises} />
-//       </div>
-//     );
-//   };
-
-//   const Total = (props) => {
-//     console.log(props);
-//     return (
-//       <p>
-//         Number of exercises{" "}
-//         {props.parts[0].exercises +
-//           props.parts[1].exercises +
-//           props.parts[2].exercises}
-//       </p>
-//     );
-//   };
+const Content = ({ parts }) => {
+  return (
+    <>
+      {parts.map((part) => (
+        <Part key={part.id} part={part.name} exercises={part.exercises} />
+      ))}
+    </>
+  );
+};
 
 const Course = ({ course }) => {
   return (
     <div>
       <Header course={course.name} />
+      <Content parts={course.parts} />
     </div>
   );
 };
