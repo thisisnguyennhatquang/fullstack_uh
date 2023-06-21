690ab69d7738bfe82f25370f4d8465893886f389
thisisnguyennhatquang <92165891+thisisnguyennhatquang@users.noreply.github.com>
2023-06-21 20:52:20 +0700

    Modified App.js and finished Course header

--- part1/courseinfo/src/components/Course.js
+++ part1/courseinfo/src/components/Course.js
@@ -1,6 +1,46 @@
-const Course = ({ props }) => {
-  const { course } = props;
-  return <li>{course.content}</li>;
+const Header = (props) => {
+  console.log(props);
+  return <h1>{props.course}</h1>;
+};
+
+//   const Part = (props) => {
+//     console.log(props);
+//     return (
+//       <p>
+//         {props.part} {props.exercises}
+//       </p>
+//     );
+//   };
+
+//   const Content = (props) => {
+//     console.log(props);
+//     return (
+//       <div>
+//         <Part part={props.parts[0].name} exercises={props.parts[0].exercises} />
+//         <Part part={props.parts[1].name} exercises={props.parts[1].exercises} />
+//         <Part part={props.parts[2].name} exercises={props.parts[2].exercises} />
+//       </div>
+//     );
+//   };
+
+//   const Total = (props) => {
+//     console.log(props);
+//     return (
+//       <p>
+//         Number of exercises{" "}
+//         {props.parts[0].exercises +
+//           props.parts[1].exercises +
+//           props.parts[2].exercises}
+//       </p>
+//     );
+//   };
+
+const Course = ({ course }) => {
+  return (
+    <div>
+      <Header course={course.name} />
+    </div>
+  );
 };
 
 export default Course;
