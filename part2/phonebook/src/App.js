4a45d9f2094ce8399e9a01276355dda2a471b4ee
thisisnguyennhatquang <92165891+thisisnguyennhatquang@users.noreply.github.com>
2023-06-26 17:05:17 +0700

    Initialized input form

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -30,6 +30,9 @@ const App = () => {
           name: <input value={newName} onChange={handleNameChange} />
         </div>
         <div>
+          number: <input />
+        </div>
+        <div>
           <button type="submit">add</button>
         </div>
       </form>
