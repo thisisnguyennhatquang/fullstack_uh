17f13b15b02b4d20f05055355e007ab53bacec66
thisisnguyennhatquang <92165891+thisisnguyennhatquang@users.noreply.github.com>
2023-07-26 19:21:21 +0700

    Added null condition (2.18)

--- part2/countries/src/components/Display.js
+++ part2/countries/src/components/Display.js
@@ -1,5 +1,7 @@
 const Display = ({ countries }) => {
-  if (countries.length > 10) {
+  if (countries.length === 0) {
+    return null;
+  } else if (countries.length > 10) {
     return `Too many matches, specify another filter`;
   } else if (countries.length > 1 && countries.length <= 10) {
     return countries.map((country) => <li>{country.name.common}</li>);
